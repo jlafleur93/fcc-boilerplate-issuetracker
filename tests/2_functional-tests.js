6d046a6751e58ad9cfb8d589d39429aaabd84687
jlafleur93 <josephlafleur@hotmail.com>
2022-02-12 20:43:21 -0700

    first commit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,22 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  suite("create an issue with every field in the post request", function(){
+      test("check that all fields are with the issue", function(){
+          chai.request(server).post('/api/issues/').send({issue_title: 'yes', 
+        issue_text: 'no', 
+        created_by: "james", 
+        assigned_to: "your mom",
+        status_text: "in QA"}).end(function(err, res){
+            assert.equal(res.status, 200)
+            assert.equal(issue_title, 'yes')
+            assert.equal(issue_text, 'no')
+            assert.equal(created_by, 'james')
+            assert.equal(assigned_to, 'your mom')
+            assert.equal(status_text, 'in QA')
+            
+
+        })
+      })
+  })
 });
