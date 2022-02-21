b722a4ca543126369230256992533f032dea6551
jlafleur93 <josephlafleur@hotmail.com>
2022-02-21 01:24:20 -0700

    added not important exlcluded turn to empty strings if not included

--- routes/api.js
+++ routes/api.js
@@ -58,6 +58,7 @@ module.exports = function (app) {
     .put(function (req, res) {
       let project = req.params.project;
       let issue = issueFinder(arr, req.body["_id"], req.body)
+      
       project = issue
       for(let i = 0; i < arr.length; i++){
         if(arr[i]['_id'] === issue._id){
@@ -65,33 +66,13 @@ module.exports = function (app) {
         }
       }
       
-      res.json(arr)
+      res.json(project)
     })
 
     .delete(function (req, res) {
       let project = req.params.project;
     });
 };
-const issues = [
-  {
-    issue_title: 'zztop',
-    issue_text: 'da fasd',
-    assigned_to: 'joe qwe',
-    created_by: 'asd',
-    status_text: 'open',
-    _id: '8672761',
-    open: true
-  },
-  {
-    issue_title: 'll',
-    issue_text: 'da fasd',
-    assigned_to: 'joe qwe',
-    created_by: 'asd',
-    status_text: 'open',
-    _id: '8852220',
-    open: true
-  },
-]
 
 function issueFinder(issue, id,options){
   let opt = ["issue_title",
@@ -106,6 +87,11 @@ function issueFinder(issue, id,options){
     if(options){
       let ret = issue.filter(x => x._id === id)
       ret = options
+      for(let i = 0; i < opt.length; i++){
+        if(options[opt[i]] === undefined){
+          ret[opt[i]] = ""
+        }
+      }
       return ret
     }
     let ret = issue.filter(x => x._id === id)
@@ -125,13 +111,3 @@ function issueFinder(issue, id,options){
   }
   console.error("required field(s) missing")
 }
-const options = {
-  issue_title: 'newVal',
-    issue_text: 'dad',
-    assigned_to: 'joe king',
-    created_by: 'deez nuts',
-    status_text: 'closed due to aids',
-}
-let newFind = issueFinder(issues, "ds0")
-let str = "asdf"
-console.log(newFind)
